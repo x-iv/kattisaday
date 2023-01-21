5e5b56195912399255cddae9409f779fddbc6e30
x-iv <104267790+x-iv@users.noreply.github.com>
2023-01-21 22:34:58 +0000

    Add files via upload

---
+++ carrots.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+
+# read the next n lines of input (contestant descriptions)
+for _ in range(n):
+    contestant_description = input()
+
+#print the number of huffle-puff problems solved
+print(k)
